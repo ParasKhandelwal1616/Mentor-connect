b3ed2c10e0690e03862def7db64b3f030cc2ec95
Paras khandelwal <127886667+ParasKhandelwal1616@users.noreply.github.com>
2025-02-26 14:53:03 +0530

    Initial commit

---
+++ mentor-connect/src/components/TeacherLogin.jsx
@@ -0,0 +1,13 @@
+export default function TeacherLogin() {
+  return (
+    <div className="flex flex-col items-center text-center p-8 bg-white rounded-2xl shadow-xl">
+      <h2 className="text-3xl font-bold mb-6 text-blue-600">Teacher Login</h2>
+      <form className="space-y-6 w-full px-8">
+        <input type="email" placeholder="Email" className="border p-3 text-black rounded-xl w-full shadow-md focus:ring-2 focus:ring-blue-500" required />
+        <input type="password" placeholder="Password" className="border text-black p-3 rounded-xl w-full shadow-md focus:ring-2 focus:ring-blue-500" required />
+        <input type="text" placeholder="Teacher ID" className="border p-3 text-black rounded-xl w-full shadow-md focus:ring-2 focus:ring-blue-500" required />
+        <button className="bg-blue-600 text-white px-6 py-3 rounded-xl shadow-md w-full hover:scale-105 transition">Login</button>
+      </form>
+    </div>
+  );
+}
